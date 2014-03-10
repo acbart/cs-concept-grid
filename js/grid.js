7f883dbe2d859c0241680a14b8b4f5cee7fa40be
acbart <acbart@vt.edu>
2014-03-09 23:11:54 -0400

    Initial navigation, improved dragging concepts.

--- js/grid.js
+++ js/grid.js
@@ -9,6 +9,7 @@ function unsetConcept(id) {
 
 function resizeCSGrid() {
     $(".cs-grid").height($(window).height());
+    $("#dashboard").height($(window).height());
     origin = getBoundingBox(".cs-grid");
     for (concept in concepts) {
         if (concepts[concept].dropped) {
@@ -25,6 +26,13 @@ function getBoundingBox(obj) {
             "y" : $(obj).offset().top,
             "width" : $(obj).width(),
             "height" : $(obj).height()};
+}
+
+function layoutCurrentConcepts() {
+    $("#concepts .hidden-list div").each(function () {
+        id = $(this).attr("concept-id");
+        $("#concept-"+id).offset($("#concept-padding-"+id).offset());
+    });
 }
 
 function buildConceptDialog() {
@@ -49,6 +57,11 @@ function buildConceptDialog() {
                 id =  $("#concept-dialog").attr("concept-id");
                 name = concept_ids[id];
                 concepts[name].comment = $("#concept-dialog textarea").val();
+                if (concepts[name].comment != "") {
+                    $(concepts[name].ref).css({"font-style" : "italic"});
+                } else {
+                    $(concepts[name].ref).css({"font-style" : "normal"});
+                }
                 $(this).dialog("close");
             }}
         ]
@@ -64,7 +77,11 @@ function createNewDraggableConcept(name, id, comment) {
                       "name": name,
                       "id": id,
                       "comment": comment};
-    concept = $("<span id='concept-"+id+"' class='concept'>"+name+"</span>");
+    $("#concepts .hidden-list").append("<div id='concept-padding-"+id+"' concept-id='"+id+"'>"+name+"</div>");
+    concept = $("<span id='concept-"+id+"' class='concept' concept-id='"+id+"'>"+name+"</span>");
+    if (comment != "") {
+        concept.css({"font-style" : "italic"});
+    }
     concept.draggable({
         distance: 10,
         scroll: false,
@@ -80,8 +97,8 @@ function createNewDraggableConcept(name, id, comment) {
             origin = getBoundingBox(".cs-grid");
             newX = self.x;
             newY = self.y;
-            x = (self.x - origin.x) - self.width / 2;
-            y = (self.y - origin.y) - self.height / 2;
+            x = (self.x - origin.x);
+            y = (self.y - origin.y);
             if (x < 0) {
                 newX = origin.x;
             }
@@ -102,6 +119,8 @@ function createNewDraggableConcept(name, id, comment) {
             concepts[name].x = (self.x - origin.x + self.width/2) / origin.width;
             concepts[name].y = (self.y - origin.y + self.height/2) / origin.height;
             concepts[name].dropped = true;
+            $("#concept-padding-" +id).remove();
+            layoutCurrentConcepts();
         }});
     concept.dblclick(function() {
         if (concepts[name].dropped) {
@@ -117,8 +136,14 @@ function createNewDraggableConcept(name, id, comment) {
             $("#concept-dialog").dialog("open");
         }
     });
+    function sleep(ms) {
+      var start = new Date().getTime(), expire = start + ms;
+      while (new Date().getTime() < expire) { }
+      return;
+    }
     concept.css({"position": "absolute"});
     $("#concepts").append(concept);
+    layoutCurrentConcepts();
 }
 
 $(document).ready(function() {
@@ -126,11 +151,8 @@ $(document).ready(function() {
     resizeCSGrid();
     buildConceptDialog();
     createNewDraggableConcept("Closures", 1, "My comment");
+    createNewDraggableConcept("Copy Constructors", 2, "");
     createNewDraggableConcept("Pointers", 100, "A longer comment");
     createNewDraggableConcept("Reading from a file", 77, "This is a much much longer comment. It should be just enough. This is a much much longer comment. It should be just enough.");
     createNewDraggableConcept("Local variables vs. global variables", 870, "");
-    createNewDraggableConcept("Local variables vs. global variable", 871, "");
-    createNewDraggableConcept("Local variables vs. global", 872, "");
-    createNewDraggableConcept("Local variables vs.", 873, "");
-    createNewDraggableConcept("Local varia", 874, "");
 });
